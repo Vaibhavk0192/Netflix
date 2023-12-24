66af052247570be4278ca9d1cd86ae60aa0dd5d5
Vaibhavk0192 <vkapoor2_be21@thapar.edu>
2023-12-25 01:06:09 +0530

    use client bug solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,10 @@
-import  Navbar  from "../components/Navbar";
-
 "use client";
-import userCurrentUser from "@/hooks/useCurrentuser";
-import { useSession, signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
-  const { data: session, status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect("/auth");
-    },
-  });
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
-
-  const { data: currentUser } = userCurrentUser();
-
   return (
     <>
-
       <Navbar />
-      <p className="text-white">
-        logged in as: {currentUser?.currentUser?.email}
-      </p>
-      <button
-        className="h-10 w-full bg-white"
-        onClick={() => {
-          signOut();
-        }}
-      >
-        LogOut
-      </button>
     </>
   );
 }
