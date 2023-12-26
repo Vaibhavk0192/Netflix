5f4f829a4d71590df8341605904e743d896755a6
gitShelly <avishigupta0719@gmail.com>
2023-12-27 01:54:55 +0530

    Billboard and movie list

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import Navbar from "../components/Navbar";
+import BillBoard from "../components/BillBoard";
+import MovieList from "../components/MovieList";
+
 
 export default function Home() {
+// video time 2:54:22
   return (
     <>
       <Navbar />
+      <BillBoard/>
+      <div className="pb-40">
+      <MovieList title="trending Now"/>
+      </div>
     </>
   );
 }
