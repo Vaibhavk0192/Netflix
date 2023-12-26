540740482b2359feafab4a6173c72226ec4f1916
gitShelly <avishigupta0719@gmail.com>
2023-12-26 22:28:12 +0530

    initial commit

--- app/(routes)/profile/page.tsx
+++ app/(routes)/profile/page.tsx
@@ -1,20 +1,55 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { useCallback } from "react";
+import { NextPageContext } from "next";
+
+// import useCurrentUser from "@/hooks/useCurrentUser";
+
+const images = [
+  '/images/default-blue.png',
+  '/images/default-red.png',
+  '/images/default-slate.png',
+  '/images/default-green.png'
+]
+interface UserCardProps {
+  name: string;
+}
+
+const UserCard: React.FC<UserCardProps> = ({ name }) => {
+  const imgSrc = images[Math.floor(Math.random() * 4)];
+
+  return (
+    <div className="group flex-row w-44 mx-auto">
+        <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+          <img draggable={false} className="w-max h-max object-contain" src={imgSrc} alt="" />
+        </div>
+      <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">{name}</div>
+   </div>
+  );
+}
+
 
 const Profile = () => {
-  const { data: session,status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect("/auth");
-    },
-  });
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
+  // const { data: session,status } = useSession({
+  //   required: true,
+  //   onUnauthenticated() {
+  //     redirect("/auth");
+  //   },
+  // });
+  // if (status === "loading") {
+  //   return <p>Loading...</p>;
+  // }
   return (
-    <div>
-      <p className="text-white text-4xl">Profiles</p>
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">Who&#39;s watching?</h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={() => selectProfile()}>
+            {/* <UserCard name={currentUser?.name} /> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,50 @@
 "use client"
+import AccountMenu from "./AccountMenu"
 import NavbarItem from "./NavbarItems"
 import {BsChevronDown,BsSearch,BsBell} from "react-icons/bs"
 import MobileMenu from "./MobileMenu";
-import {useState , useCallback} from "react"
+import  {useState , useCallback,useEffect} from "react"
+
+// how much the use is going o scroll down
+const TOP_OFFSET=66;
 
 const Navbar = () => {
   const [ShowMobileMenu, setShowMobileMenu] = useState(false)
+  const [ShowAccountMenu, setShowAccountMenu] = useState(false)
+  const [ShowBackground, setShowBackground] = useState(false)
+
   const toggleMobileMenu=useCallback(
     () => {
       setShowMobileMenu((current)=>!current)
     },
     [],
   )
+
+  const toggleAccountMenu=useCallback(
+    () => {
+      setShowAccountMenu((current)=>!current)
+    },
+    [],
+  )
+  
+  useEffect(() => {
+    const handleScroll=()=>{
+      if(window.scrollY>=TOP_OFFSET){
+        setShowBackground(true);
+      }else{
+        setShowBackground(false);
+      }
+    }
+    window.addEventListener("scroll",handleScroll);
+    return ()=>{
+      window.removeEventListener("scroll",handleScroll);
+    }
+  }, [])
   
 
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-4 md:px-4 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+      <div className={`px-4 md:px-4 py-6 flex flex-row items-center transition duration-500 ${ShowBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
         <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
             <NavbarItem label="Home"/>
@@ -28,7 +56,7 @@ const Navbar = () => {
         </div>
         <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
             <p className="text-white text-sm">Browse</p>
-            <BsChevronDown className="text-white transition"/>
+            <BsChevronDown className={`text-white transition ${ShowMobileMenu ? "rotate-180":"rotate-0"}`}/>
             <MobileMenu visible={ShowMobileMenu}/>
         </div>
         <div className="flex flex-row ml-auto mr-4 gap-7 items-center">
@@ -38,12 +66,13 @@ const Navbar = () => {
             <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
             <BsBell/>
             </div>
-            <div className="flex flex-row  gap-2 items-center cursor-pointer relative">
+            <div onClick={toggleAccountMenu} className="flex flex-row  gap-2 items-center cursor-pointer relative">
                <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
                 <img src="/images/default-blue.png" alt=""/>
                </div>
+               <BsChevronDown className={`text-white transition ${ShowAccountMenu ? "rotate-180":"rotate-0"}`}/>
+               <AccountMenu visible={ShowAccountMenu}/>
             </div>
-
         </div>
       </div>
     </nav>
