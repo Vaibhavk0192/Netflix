bed6cc0ce9c31a61424dcd078b7c416328760971
gitShelly <avishigupta0719@gmail.com>
2023-12-27 02:04:02 +0530

    Merge branch 'master' of https://github.com/Vaibhavk0192/Netflix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 "use client";
 import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar"
+import BillBoard from "../components/BillBoard"
+import MovieList from "../components/MovieList"
+
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
 
 export default function Home() {
+    const { data: session,status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/auth");
+    },
+  });
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
   return (
     <>
       <Navbar />
