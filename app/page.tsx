27030d0bab7a8db771e518107292d2142a8b0a59
gitShelly <avishigupta0719@gmail.com>
2023-12-27 21:07:58 +0530

    new component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Navbar from "../components/Navbar";
-import Navbar from "../components/Navbar"
 import BillBoard from "../components/BillBoard"
 import MovieList from "../components/MovieList"
 
