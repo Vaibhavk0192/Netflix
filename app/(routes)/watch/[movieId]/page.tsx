d78b79f87a401230384f3afb7d8f023da536edbf
gitShelly <avishigupta0719@gmail.com>
2024-01-06 16:02:59 +0530

    change

--- app/(routes)/watch/[movieId]/page.tsx
+++ app/(routes)/watch/[movieId]/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 
-import { useParams } from "next/navigation";
+import { useParams,useRouter } from "next/navigation";
 
 import useMovie from "@/hooks/useMovie";
 
@@ -12,11 +12,14 @@ const Watch = () => {
   const movieId = params.movieId;
   const { data } = useMovie(movieId);
   console.log(data);
+  const router=useRouter();
 
   return (
     <div className="h-screen w-screen bg-black">
       <nav className=" fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-opacity-70">
-        <AiOutlineArrowLeft className="text-white" size={30} />
+        <AiOutlineArrowLeft className="text-white cursor-pointer" size={30} onClick={()=>{
+        router.back()
+        }} />
         <p className="text-white text-1xl md:text-3xl font-bold">
           <span className="font-light mr-2">Watching:</span>
           {data?.title}
